4130cfbbef7b9dab36b17ea2ec468230cbce612b
LootDelivery <stresstreets2@gmail.com>
2025-02-26 00:50:09 +0500

    Add files via upload

---
+++ mods/B42 Rain's Axes & Blades/42/media/lua/shared/Definitions/AttachedWeaponDefinitions_RainsAxesAndBlades.lua
@@ -0,0 +1,37 @@
+require 'Items/AttachedWeaponDefinitions'
+
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "RainsToolsandBlades.MacheteBush")
+table.insert(AttachedWeaponDefinitions["MacheteShoulder"].weapons, "RainsToolsandBlades.MacheteBush")
+table.insert(AttachedWeaponDefinitions["macheteInBack"].weapons, "RainsToolsandBlades.MacheteBush")
+table.insert(AttachedWeaponDefinitions["meleeInBack_Bandit"].weapons, "RainsToolsandBlades.MacheteBush")
+table.insert(AttachedWeaponDefinitions["weaponInStomach"].weapons, "RainsToolsandBlades.MacheteBush")
+
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "RainsToolsandBlades.SawbackMachete")
+table.insert(AttachedWeaponDefinitions["MacheteShoulder"].weapons, "RainsToolsandBlades.SawbackMachete")
+table.insert(AttachedWeaponDefinitions["macheteInBack"].weapons, "RainsToolsandBlades.SawbackMachete")
+table.insert(AttachedWeaponDefinitions["meleeInBack_Bandit"].weapons, "RainsToolsandBlades.SawbackMachete")
+table.insert(AttachedWeaponDefinitions["weaponInStomach"].weapons, "RainsToolsandBlades.SawbackMachete")
+
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "RainsToolsandBlades.CurvedMachete")
+table.insert(AttachedWeaponDefinitions["MacheteShoulder"].weapons, "RainsToolsandBlades.CurvedMachete")
+table.insert(AttachedWeaponDefinitions["macheteInBack"].weapons, "RainsToolsandBlades.CurvedMachete")
+table.insert(AttachedWeaponDefinitions["meleeInBack_Bandit"].weapons, "RainsToolsandBlades.CurvedMachete")
+table.insert(AttachedWeaponDefinitions["weaponInStomach"].weapons, "RainsToolsandBlades.CurvedMachete")
+
+table.insert(AttachedWeaponDefinitions["meatCleaverBackLowQuality"].weapons, "RainsToolsandBlades.Handy")
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "RainsToolsandBlades.Handy")
+table.insert(AttachedWeaponDefinitions["lumberjack"].weapons, "RainsToolsandBlades.Handy")
+table.insert(AttachedWeaponDefinitions["weaponInStomach"].weapons, "RainsToolsandBlades.Handy")
+
+table.insert(AttachedWeaponDefinitions["meatCleaverBackLowQuality"].weapons, "RainsToolsandBlades.Bearded")
+table.insert(AttachedWeaponDefinitions["meatCleaverBack"].weapons, "RainsToolsandBlades.Bearded")
+table.insert(AttachedWeaponDefinitions["lumberjack"].weapons, "RainsToolsandBlades.Bearded")
+table.insert(AttachedWeaponDefinitions["weaponInStomach"].weapons, "RainsToolsandBlades.Bearded")
+
+table.insert(AttachedWeaponDefinitions["axeBack"].weapons, "RainsToolsandBlades.Axy")
+table.insert(AttachedWeaponDefinitions["meleeInBack_Bandit"].weapons, "RainsToolsandBlades.Axy")
+table.insert(AttachedWeaponDefinitions["weaponInStomach"].weapons, "RainsToolsandBlades.Axy")
+
+table.insert(AttachedWeaponDefinitions["katanaStomach"].weapons, "RainsToolsandBlades.KatanaB")
+table.insert(AttachedWeaponDefinitions["katanaStomach"].weapons, "RainsToolsandBlades.Wakizashi")
+table.insert(AttachedWeaponDefinitions["katanaStomach"].weapons, "RainsToolsandBlades.WakizashiB")
